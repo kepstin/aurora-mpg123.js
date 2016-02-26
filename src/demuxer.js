6307ae73d175bed7435851853977cb21794799ca
Calvin Walton <calvin.walton@kepstin.ca>
2016-02-25 22:51:45 -0500

    Emit "format" message in Demuxer
    The demuxer has to emit a "format" message so that the correct encoder
    will be selected. Since I have a basic MP3 frame parser for probing,
    extend it to pull out the minimum information required for a useful
    format message (codec, sample rate, sample format, channels)

--- src/demuxer.js
+++ src/demuxer.js
@@ -3,9 +3,7 @@ var AV = require('av');
 var MP3Demuxer = AV.Demuxer.extend(function() {
 	AV.Demuxer.register(this);
 
-	this.probe = function(stream) {
-		// The MP3 probing is mostly ad-hoc matching. You don't really
-		// know if something is MP3 until you (fail to) decode it.
+	this.getFormatInfo = function(stream) {
 		var off = stream.offset;
 
 		// Check for an ID3v2 header at the start of the file
@@ -30,22 +28,61 @@ var MP3Demuxer = AV.Demuxer.extend(function() {
 		var sync = stream.readUInt16(false);
 		if (sync & 0xFFFE != 0xFFFA) {
 			stream.seek(off);
-			return false;
+			return null;
 		}
+
+		var rate = stream.readUint8();
 
 		// Check for a valid bitrate
-		var rate = stream.readUint8() & 0xF0;
-		if (rate < 0x10 || rate > 0xE0) {
+		if (rate & 0xF0 < 0x10 || rate & 0xF0 > 0xE0) {
 			stream.seek(off);
-			return false;
+			return null;
 		}
 
-		// It's... probably an MP3 (MPEG-1 Layer 3), I guess?
-		return true;
+		// Determine the sample rate
+		var sr = 0;
+		if (rate & 0x0C == 0x00)
+			sr = 44100;
+		else if (rate & 0x0C == 0x04)
+			sr = 48000;
+		else if (rate & 0x0C ==0x08)
+			sr = 32000;
+		else {
+			stream.seek(off);
+			return null;
+		}
+
+		return {
+			formatID: "mp3",
+			sampleRate: sr,
+			channelsPerFrame: 2, /* Decoder hardcoded to output 2 channels */
+			floatingPoint: true
+		};
+	}
+
+	this.probe = function(stream) {
+		// The MP3 probing is mostly ad-hoc matching. You don't really
+		// know if something is MP3 until you (fail to) decode it.
+
+		var info = this.getFormatInfo(stream);
+
+		if (info != null) {
+			// It's... probably an MP3 (MPEG-1 Layer 3), I guess?
+			return true;
+		} else {
+			return false;
+		}
 	};
 
 	this.prototype.readChunk = function() {
 		var stream = this.stream;
+
+		// Need to parse enough of the file to get format & samplerate
+		// so that the decoder can be selected (and used)
+		if (!this.sentInfo) {
+			this.emit("metadata", this.getFormatInfo(stream));
+			this.sentInfo = true;
+		}
 
 		// MP3 isn't really muxed; just hand the data straight to
 		// the decoder.
